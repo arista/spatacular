688b93492ee49c94ccf80f03eb0410ab22db7a8e
Nathan Abramson <arista@abramsons.info>
2026-03-08 20:09:05 +0000

    route defs

--- PLANS.md
+++ PLANS.md
@@ -4,7 +4,13 @@ This document tracks the roadmap and planned features for brint
 
 ## Current Focus
 
-Please look at overiew.md and give me your initial thoughts.  We're still in early stages of thinking about this.
+We've done a little work on an [overview](./docs/overview.md), and you've provided some response in [overview-analysis](./docs/overview-analysis.md).
+
+I've started thinking about what the "contract" might look like.  I'm assuming there will be a need to describe request and response types, effectively JSON payloads.  So I've started sketching out what that might look like in [contract-sketch](./docs/contract-sketch.ts).
+
+I'm particularly interested in having type declarations that can drive validation, string TS type declarations, and eventually OpenAPI declarations.
+
+I've gotten farther on the contract, now defining routes and implying what might be driving by those definitions.  Please take a look.
 
 ## Docs and References
 
--- docs/contract-sketch.ts
+++ docs/contract-sketch.ts
@@ -32,7 +32,7 @@ type StringTypeDecl = {type: "String"}
 type ArrayTypeDecl = {type: "Array", elementType: TypeDecl}
 
 type ObjectTypeDecl = {type: "Object", properties: Record<string, ObjectPropertyDecl>}
-
+v
 type ObjectPropertyDecl = {propertyType: TypeDecl, optional: boolean}
 
 type TupleTypeDecl = {type: "Tuple", elements: Array<TypeDecl>}
@@ -45,3 +45,66 @@ type IntersectionTypeDecl = {type: "Intersection", types: Array<TypeDecl>}
 type DiscriminatedUnionTypeDecl = {type: "DiscriminatedUnion", discriminator: string, types: Array<TypeDecl>}
 
 type NamedTypeRef = {type: "NamedType", typeName: string}
+
+
+// Routes define the contract between server and client.  Routes are declared with internal names, which are used to generate API and type names.  Routes can also be organized into groups, which both provide multiple routes with a common prefix (and potentially a common set of params), and also a mechanism for organizing the internal types and API definitions.
+
+// The generated route Request and Response types are used by both server and client.  The type names are formed from the route names - i.e., AddUserRequest / AddUserResponse.  If groups are used, then those groups organize the types into separate modules: e.g., users.AddRequest / users.AddResponse
+
+// Each RouteDef specifies a path, which is parsed using the "path-to-regexp" npm library's parse() call.  The parsed path will reveal the parameters defined by the path (which should be combined with any params found in group prefixes - error if collisions).  If there are any parameters, these will form the "params" property of the generated Request type.  The Request type can also specify additional query, headers, and body types.
+
+// Mapping from route or group name to entry
+type Routes = Record<string, RoutesEntry>
+
+type RoutesEntry =
+  RouteGroup
+  | RouteDef
+
+// The path should be a path that can be interpreted by the "path-to-regexp" npm library.
+type RouteGroup = {
+  type: "Group"
+  path: string
+  routes: Routes
+}
+
+// The path should be a path that can be interpreted by the "path-to-regexp" npm library.
+type RouteDef = {
+  type: "Route"
+  // No method means match any method
+  method?: RouteMethod | Array<RouteMethod>
+  path: string
+  request?: RouteRequestType
+  response?: RouteResponseType
+}
+
+// Maybe someday add PUT, PATCH, DELETE
+type RouteMethod = "GET" | "POST"
+
+// Query, and Headers must resolve to ObjectTypes.  The param types don't need to be specified since they are inferred from the route's path declaration
+type RouteRequestType = {
+  query?: TypeDecl
+  headers?: TypeDecl
+  body?: TypeDecl
+}
+
+type RouteResponseType = {
+  body?: TypeDecl
+}
+
+// With these route definitions, both client and server interfaces can be defined.  On the client side, each request gets its own method, named using the fully-qualified route:
+//
+//   clientApi.users.acoount.paymentInfo.UpdateCC(request: UpdateCCRequest): Promise<UpdateCCResponse>
+//
+// On the server side, a similar API interface is generated that the app is expected to implement.
+//
+//   serverApi.users.acoount.paymentInfo.UpdateCC(request: UpdateCCRequest): Promise<UpdateCCResponse>
+//
+// If groups along the way define params, those will show up at the appropriate level:
+//
+//   serverApi.users(params: usersParams).acoount.paymentInfo.UpdateCC(request: UpdateCCRequest): Promise<UpdateCCResponse>
+//
+// This allows the server to create request handling objects along the way that can process parameters as needed
+//
+// TBD - the call to a server api might also include additional context information, and the response might allow the implementation to declare contextual information about the response, such as setting cookies, etc.
+//
+
